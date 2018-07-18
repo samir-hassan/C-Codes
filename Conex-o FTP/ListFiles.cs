563e3c35a268e87e2db284e9be95ee635511496e
samir-hassan <31141202+samir-hassan@users.noreply.github.com>
2018-07-18 15:10:21 -0300

    Create ListFiles.cs

---
+++ Conex-o FTP/ListFiles.cs
@@ -0,0 +1,23 @@
+-- Listar Arquivos que estao dentro do FTP
+
+public static List<string> ListFiles(string username, string password, string server)
+		{
+			FtpWebRequest ftpRequest = (FtpWebRequest)WebRequest.Create(new Uri(server));
+			ftpRequest.Credentials = new NetworkCredential(username, password);
+			ftpRequest.Method = WebRequestMethods.Ftp.ListDirectory;
+			FtpWebResponse response = (FtpWebResponse)ftpRequest.GetResponse();
+			StreamReader streamReader = new StreamReader(response.GetResponseStream());
+
+			List<string> directories = new List<string>();
+
+			string line = streamReader.ReadLine();
+			while (!string.IsNullOrEmpty(line))
+			{
+				directories.Add(line);
+				line = streamReader.ReadLine();
+			}
+
+			streamReader.Close();
+
+			return directories;
+		}
